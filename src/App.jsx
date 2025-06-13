1d9a35f8c5306b0be1972c3705568a6f475612c5
dmitri <aw3s0m3ss@gmail.com>
2025-06-13 18:09:14 +0300

    added tailwind defined routes added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import MyBookings from "./pages/MyBookings";
+import NavBar from "./components/NavBar";
+import SeatLayout from "./pages/SeatLayout";
+import Favorite from "./pages/Favorite";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      {!isAdminRoute && <NavBar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+       {!isAdminRoute && <Footer />}
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div><h1 className='text-red-600'>navbar</h1></div>
+  )
+}
+
+export default NavBar
