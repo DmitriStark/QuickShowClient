ec465d3c12c3ac44a580e05b454849a2aac4e21b
dmitri <aw3s0m3ss@gmail.com>
2025-06-18 20:32:14 +0300

    completed movies page and fovorites made dummy

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
 
 const MovieDetails = () => {
-  return (
-    <div>MovieDetails</div>
-  )
-}
+  const { id } = useParams();
+  const [show, setShows] = useState(null);
 
-export default MovieDetails
+  const getShow = async () => {
+    const show = dummyShowsData.find((show) => show._id === id);
+    setShows({
+      movie: show,
+      dateTime: dummyDateTimeData,
+    });
+  };
+
+  useEffect(() => {
+    getShow();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
+  return show ? (<div>MovieDetails</div>) :(<div>Loading...</div>) 
+};
+
+export default MovieDetails;
